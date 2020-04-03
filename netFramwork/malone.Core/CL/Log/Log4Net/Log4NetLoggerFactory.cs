ff0af02a2e73206fc122400adb08fb8b610cc331
malonejv <malonejv@gmail.com>
2020-04-03 14:27:13 -0300

    Archivos de log

---
+++ netFramwork/malone.Core/CL/Log/Log4Net/Log4NetLoggerFactory.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using malone.Core.CL.Log;
+
+namespace malone.Core.CL.Log.Log4Net
+{
+    public class Log4NetLoggerFactory : LoggerFactory
+    {
+        static Log4NetLoggerFactory()
+        {
+            // load the log4net configuration from the application configuration.
+            XmlConfigurator.Configure();
+        }
+        
+        public override ILogger GetLogger()
+        {
+            return new Log4netLogger(LogManager.GetLogger(typeof(ILog)));
+        }
+
+        public override ILogger GetLogger(string name)
+        {
+            return new Log4netLogger(LogManager.GetLogger(name));
+        }
+    }
+}
