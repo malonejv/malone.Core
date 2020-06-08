16e61a7a876c6e94865c91ee4637900483d1015c
jlopezmalone <jlopezmalone@santandertecnologia.com.ar>
2020-06-08 10:59:52 -0300

    Correccion de nuspec

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyTitle("malonejv.Core")]
 [assembly: AssemblyCompany("Javier López Malone")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
