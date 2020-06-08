098d6cf0ef0fbb35b13aff609c2079fc325cd44b
jlopezmalone <jlopezmalone@santandertecnologia.com.ar>
2020-06-08 09:52:13 -0300

    Modifico Build Events

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Reflection;
 [assembly: AssemblyTitle("malonejv.Core")]
 [assembly: AssemblyCompany("Javier López Malone")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("2.0.0")]
--- src/malone.Core.AdoNet.Test/Properties/AssemblyInfo.cs
+++ src/malone.Core.AdoNet.Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("390a9f3c-ca35-4d16-ab53-e8bf757777fd")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.2")]
+
 
