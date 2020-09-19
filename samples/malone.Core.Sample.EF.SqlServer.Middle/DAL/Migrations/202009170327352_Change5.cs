408b8c0b25c22cd63f3cdb0904dc41673bea0306
jlopezmalone <jlopezmalone@santandertecnologia.com.ar>
2020-09-19 02:58:27 -0300

    Arreglos en Sample EF SqlServer

---
+++ samples/malone.Core.Sample.EF.SqlServer.Middle/DAL/Migrations/202009170327352_Change5.cs
@@ -0,0 +1,22 @@
+﻿namespace malone.Core.Sample.EF.SqlServer.Middle.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Change5 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TodoLists", "User_Id", c => c.Int());
+            CreateIndex("dbo.TodoLists", "User_Id");
+            AddForeignKey("dbo.TodoLists", "User_Id", "dbo.Users", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TodoLists", "User_Id", "dbo.Users");
+            DropIndex("dbo.TodoLists", new[] { "User_Id" });
+            DropColumn("dbo.TodoLists", "User_Id");
+        }
+    }
+}
