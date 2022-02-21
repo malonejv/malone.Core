4364def6781f4f1f118220c274caee52d8c4b2b4
malonejv <malonejv@gmail.com>
2022-02-21 21:59:11 +0100

    #58

--- src/BuildScripts/Post-BuildEvent.ps1
+++ src/BuildScripts/Post-BuildEvent.ps1
@@ -11,14 +11,17 @@ Environment in which the version number would be updated (by default 'Developmen
 .PARAMETER Configuration
 Configuration of build process (by default Debug).
  
-.PARAMETER $BuildArtifactsPath
+.PARAMETER BuildArtifactsPath
 Path where the build-artifacts where generated.
  
 .PARAMETER OutputDir
 Path where the pack-artifacts will be copied.
- 
+
+.PARAMETER ShouldPack
+Where the packing process should execute.
+
 .EXAMPLE
-.\Post-BuildEvent -SolutionPath $SolutionPath -Environment $Environment -Configuration $Configuration -OutputDir $OutputDir'
+.\Post-BuildEvent -SolutionPath $SolutionPath -Environment $Environment -Configuration $Configuration -BuildArtifactsPath $BuildArtifactsPath -OutputDir $OutputDir -ShouldPack $ShouldPack'
  
 #>
 [CmdletBinding()]
@@ -36,79 +39,81 @@ param (
     [ValidateScript({ Test-Path -Path $_ -PathType Container })]
     [string]
     $BuildArtifactsPath,
-    [parameter(Mandatory=$true)]
     [string]
-    $OutputDir
+    $OutputDir,
+    [bool]
+    $ShouldPack=$false
 )
+    if($ShouldPack -eq $true){
+        $ErrorActionPreference = 'Stop'
 
-    $ErrorActionPreference = 'Stop'
-
-    Write-Verbose "Verbose: '$($PSBoundParameters['Verbose'])'"
-    if($PSBoundParameters['Verbose'] -and $VerbosePreference -ne 'Continue') {
-        Write-Verbose "VerbosePreference: $VerbosePreference"
-        $VerbosePreference = 'Continue'
-        Write-Verbose "VerbosePreference setted to: $VerbosePreference"
+        Write-Verbose "Verbose: '$($PSBoundParameters['Verbose'])'"
+        if($PSBoundParameters['Verbose'] -and $VerbosePreference -ne 'Continue') {
+            Write-Verbose "VerbosePreference: $VerbosePreference"
+            $VerbosePreference = 'Continue'
+            Write-Verbose "VerbosePreference setted to: $VerbosePreference"
     
-        Write-Host ""
-    }
+            Write-Host ""
+        }
     
-	if (!$Environment) {
-		$Environment = 'Development'
-	}
+	    if (!$Environment) {
+		    $Environment = 'Development'
+	    }
 
-    if($Environment -eq 'Local' -and $Configuration -ne "DebugNuget"){
-        return 
-	}elseif($Environment -eq 'Production'){
-        $Configuration = 'Release'
-    }else{
-        $Configuration = 'Debug'
-    }
+        if($Environment -eq 'Local' -and $Configuration -ne "DebugNuget"){
+            return 
+	    }elseif($Environment -eq 'Production'){
+            $Configuration = 'Release'
+        }else{
+            $Configuration = 'Debug'
+        }
 
-	if($PSBoundParameters['Verbose']) {
-        Write-Host ""
-	}
+	    if($PSBoundParameters['Verbose']) {
+            Write-Host ""
+	    }
 
-    $ScriptsName = [System.IO.Path]::GetFileNameWithoutExtension($($MyInvocation.MyCommand.Path | Split-Path -Leaf))
-    Write-Verbose "Script name: $ScriptsName"
+        $ScriptsName = [System.IO.Path]::GetFileNameWithoutExtension($($MyInvocation.MyCommand.Path | Split-Path -Leaf))
+        Write-Verbose "Script name: $ScriptsName"
     
-    if($PSBoundParameters['Verbose']) {
-        Write-Host ""
-	}
+        if($PSBoundParameters['Verbose']) {
+            Write-Host ""
+	    }
 
-    Write-Verbose "Input Patameters:"
-    $PSBoundParameters.GetEnumerator() | ForEach {
-            Write-Verbose "  $_"
-    }
+        Write-Verbose "Input Patameters:"
+        $PSBoundParameters.GetEnumerator() | ForEach {
+                Write-Verbose "  $_"
+        }
     
-	if($PSBoundParameters['Verbose']) {
-        Write-Host ""
-	}
+	    if($PSBoundParameters['Verbose']) {
+            Write-Host ""
+	    }
     
-    $OutputDir = ($OutputDir | Resolve-Path).ProviderPath
+        $OutputDir = ($OutputDir | Resolve-Path).ProviderPath
  
-    $PropagateVerbose=($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+        $PropagateVerbose=($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
         
-    Write-Verbose "Defined variables:"
-    Write-Verbose "  Current script: $($MyInvocation.MyCommand.Path)"
-    $ScriptsDir = $MyInvocation.MyCommand.Path | Split-Path
+        Write-Verbose "Defined variables:"
+        Write-Verbose "  Current script: $($MyInvocation.MyCommand.Path)"
+        $ScriptsDir = $MyInvocation.MyCommand.Path | Split-Path
 
-    $GetProjectsScript="$ScriptsDir\Get-ProjectsInSolution.ps1"
-    $PackProjectsScript="$ScriptsDir\Pack-Projects.ps1"
+        $GetProjectsScript="$ScriptsDir\Get-ProjectsInSolution.ps1"
+        $PackProjectsScript="$ScriptsDir\Pack-Projects.ps1"
     
-    Write-Verbose "  GetProjectsScript: $GetProjectsScript"
+        Write-Verbose "  GetProjectsScript: $GetProjectsScript"
     
-    Write-Verbose "Setting up the solution projects"
-    Write-Verbose " Command: Invoke-Expression `"$GetProjectsScript -Path `"$SolutionPath`" -Type `"NonTestProjects`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))`""
-    Invoke-Expression "$GetProjectsScript -Path `"$SolutionPath`" -Type `"NonTestProjects`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))" -Outvariable Projects
-    Write-Verbose " Projects count: $($Projects.count)"
+        Write-Verbose "Setting up the solution projects"
+        Write-Verbose " Command: Invoke-Expression `"$GetProjectsScript -Path `"$SolutionPath`" -Type `"NonTestProjects`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))`""
+        Invoke-Expression "$GetProjectsScript -Path `"$SolutionPath`" -Type `"NonTestProjects`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))" -Outvariable Projects
+        Write-Verbose " Projects count: $($Projects.count)"
     
-    $projectParam=$projects | foreach{ "$_, " }
+        $projectParam=$projects | foreach{ "$_, " }
     
-    if($Environment -eq 'Development' -or $Environment -eq 'Production'){
-        Write-Verbose "Copying build-artifacts"
-        Copy-Item "$BuildArtifactsPath" -Destination "$solutionDir" -Force -Recurse -Container
+        if($Environment -eq 'Development' -or $Environment -eq 'Production'){
+            Write-Verbose "Copying build-artifacts"
+            Copy-Item "$BuildArtifactsPath" -Destination "$solutionDir" -Force -Recurse -Container
+        }
+
+        Write-Verbose "Packing the solution projects"
+        Write-Verbose " Command: Invoke-Expression `"$PackProjectsScript -Projects `"$projectsParam`" -PackingProperties `"$PackingProperties`" -Configuration `"$Configuration`" -OutputDir `"$OutputDir`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))`""
+        Invoke-Expression "$PackProjectsScript -Projects `"$Projects`" -Configuration `"$Configuration`" -OutputDir `"$OutputDir`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))"
     }
-
-    Write-Verbose "Packing the solution projects"
-    Write-Verbose " Command: Invoke-Expression `"$PackProjectsScript -Projects `"$projectsParam`" -PackingProperties `"$PackingProperties`" -Configuration `"$Configuration`" -OutputDir `"$OutputDir`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))`""
-    Invoke-Expression "$PackProjectsScript -Projects `"$Projects`" -Configuration `"$Configuration`" -OutputDir `"$OutputDir`" -Verbose:([bool]::parse(`"$PropagateVerbose`"))"
--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -15,12 +15,12 @@ using System.Reflection;
  * como de master -> development y development -> master
  * SIEMPRE tomar la versión de development.
  */
-[assembly: AssemblyFileVersion("2.1.11.7")]
+[assembly: AssemblyFileVersion("2.1.11.17")]
 
 /*NO MODIFICAR*/
 /**************/
 [assembly: AssemblyVersion("2.1.11")]
-[assembly: AssemblyInformationalVersion("2.1.11.7-dev")]
+[assembly: AssemblyInformationalVersion("2.1.11.17-dev")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
