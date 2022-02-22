89e22cce927b7c5b3fb95b9307f5e2982eae0ed9
malonejv <malonejv@gmail.com>
2022-02-22 07:18:24 +0000

    Version updated

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -15,12 +15,12 @@ using System.Reflection;
  * como de master -> development y development -> master
  * SIEMPRE tomar la versión de development.
  */
-[assembly: AssemblyFileVersion("2.1.11.13")]
+[assembly: AssemblyFileVersion("2.1.11.14")]
 
 /*NO MODIFICAR*/
 /**************/
 [assembly: AssemblyVersion("2.1.11")]
-[assembly: AssemblyInformationalVersion("2.1.11.13-beta")]
+[assembly: AssemblyInformationalVersion("2.1.11.14-beta")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
