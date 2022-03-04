4bbbd38a5b1a3956a32934241623d152e8ae91ac
Javier Rodrigo Lopez Malone <javier.lopez@mecalux.com>
2022-03-04 12:45:27 +0100

    Refactor

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -15,12 +15,12 @@ using System.Reflection;
  * como de master -> development y development -> master
  * SIEMPRE tomar la versión de development.
  */
-[assembly: AssemblyFileVersion("2.1.11.86")]
+[assembly: AssemblyFileVersion("2.1.11.88")]
 
 /*NO MODIFICAR*/
 /**************/
 [assembly: AssemblyVersion("2.1.11")]
-[assembly: AssemblyInformationalVersion("2.1.11.86-dev")]
+[assembly: AssemblyInformationalVersion("2.1.11.88-dev")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
