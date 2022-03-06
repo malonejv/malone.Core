23cc4454785b364fa7276611aa8770d8146d7829
malonejv <malonejv@gmail.com>
2022-03-06 20:03:34 +0000

    Version updated

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,11 +1,11 @@
-//<author>Javier López Malone</author>
+//<author>Javier Lï¿½pez Malone</author>
 //<date>25/11/2020 02:48:19</date>
 
 using System.Reflection;
 
 [assembly: AssemblyTitle("malonejv.Core")]
-[assembly: AssemblyCompany("Javier López Malone")]
-[assembly: AssemblyCopyright("Copyright © Javier López Malone 2022")]
+[assembly: AssemblyCompany("Javier Lï¿½pez Malone")]
+[assembly: AssemblyCopyright("Copyright ï¿½ Javier Lï¿½pez Malone 2022")]
 
 
 /*
@@ -13,14 +13,14 @@ using System.Reflection;
  * El script se encarga de actualizar el resto.
  * Al hacer merge tanto de feature -> development
  * como de master -> development y development -> master
- * SIEMPRE tomar la versión de development.
+ * SIEMPRE tomar la versiï¿½n de development.
  */
-[assembly: AssemblyFileVersion("2.1.11.44")]
+[assembly: AssemblyFileVersion("2.1.12.0")]
 
 /*NO MODIFICAR*/
 /**************/
-[assembly: AssemblyVersion("2.1.11")]
-[assembly: AssemblyInformationalVersion("2.1.11.44-dev")]
+[assembly: AssemblyVersion("2.1.12")]
+[assembly: AssemblyInformationalVersion("2.1.12.0-beta")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
